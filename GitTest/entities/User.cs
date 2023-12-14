f3ee21bc2af6485158dbfefb126e90d9af186a07
Sy Hoai <thanghoaitk23@gmail.com>
2023-12-14 14:17:35 +0700

    Append ID into Student.cs

--- GitTest/entities/User.cs
+++ GitTest/entities/User.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace GitTest.entities
 {
     internal class User
     {
+        public int Id { get; set; }
     }
 }
